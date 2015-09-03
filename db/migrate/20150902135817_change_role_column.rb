c386c72ced64d5fa2ce7e778207d798c627a474d
jselle <jselle@cip82.informatik.uni-osnabrueck.de>
2015-09-03 10:39:04 +0200

    added user roles,changed user_role default

--- db/migrate/20150902135817_change_role_column.rb
+++ db/migrate/20150902135817_change_role_column.rb
@@ -1,7 +1,10 @@
 # set default role for new user
 
 class ChangeRoleColumn < ActiveRecord::Migration
-  def default_role
-  	change_column :users, :role, :default => "student"
+  def change
+  	change_column :users, :role, default: "student"
   end
+ #def down
+ #	change_column_default :users, :role, nil
+  #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902135817) do
+ActiveRecord::Schema.define(version: 20150903082007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,12 +63,12 @@ ActiveRecord::Schema.define(version: 20150902135817) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                                null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role"
+    t.string   "role",             default: "student"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
