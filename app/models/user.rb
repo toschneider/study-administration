173b4868228d71ae2da28ee97a8cb4368c012b01
mbeutel <mbeutel@cip78.informatik.uni-osnabrueck.de>
2015-09-03 13:42:59 +0200

    merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-# validates email and password
-class User < ActiveRecord::Base
-  authenticates_with_sorcery!
-
-=======
-
 
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
@@ -13,14 +6,11 @@ class User < ActiveRecord::Base
   ROLES = %i[admin moderator student guest]
 
   # validates password and email
->>>>>>> c386c72ced64d5fa2ce7e778207d798c627a474d
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
   validates :email, presence: true, email: true, uniqueness: true
-
-  ROLES = %i[admin moderator student guest]
 
 
 
