789143cd642718609782a9c67bd06a97bb542976
jselle <jselle@cip82.informatik.uni-osnabrueck.de>
2015-09-04 11:21:49 +0200

    user-controll for events and user-rights

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,8 +8,16 @@ class Ability
      user ||= User.new # guest user (not logged in)
         if user.role=="admin"
          can :manage, :all
-       else
-         can :read, :all
-     end
+        end
+        if user.role=="student"
+        can :read, :all
+        can :destroy,  :user_id => user.id
+        end
+        if user.role=="guest"
+         cannot :create, Event 
+         cannot :edit, :all
+         cannot :destroy, :all
+        end
   end
 end
+
