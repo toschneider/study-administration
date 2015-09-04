abe244a5861cf974dd70ed66354e0f861a0ccb8a
pklein <philipp christopher klein>
2015-09-04 14:57:14 +0200

    Creating CSV-Data, Implement CSV-Reader

---
+++ app/controllers/csv_reader_controller.rb
@@ -0,0 +1,11 @@
+# Vtecker,pklein
+# The Controller written for our CSV-Reader.
+# Just calls the import and refers back afterwards.
+class CsvReaderController < ApplicationController
+	def import
+
+  		Event.import(params[:file])
+  		# After the successful import of the CSV-File , we refer back to the current page.
+  		redirect_to :back , notice: "Die Veranstaltungen der ausgewählten CSV-Datei wurden erfolgreich in die Datenbank eingelesen. VINZENZ TECKER UND PHILIPP-CHRISTOPHER KLEIN SEI DANK"
+  	end
+end
