2c09cf8af66b689facb11758701828b5362c1985
tstahn <tstahn@cip80.informatik.uni-osnabrueck.de>
2015-09-04 12:45:02 +0200

    password validation also on user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   ROLES = %i[admin moderator student guest]
 
   # validates password and email
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
-  validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
+  validates :password, length: { minimum: 3 }, :on=>[:create, :update]
+  validates :password, confirmation: true, :on=>[:create, :update]
+  validates :password_confirmation, presence: true, :on=>[:create, :update]
 
   validates :email, presence: true, email: true, uniqueness: true
 
